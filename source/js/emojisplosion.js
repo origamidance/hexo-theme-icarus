c28239f277d95c80ef8cc4f61629044e9b585b47
origamidance <origamidance@gmail.com>
2019-06-25 15:51:02 +0200

    change avatar

--- source/js/emojisplosion.js
+++ source/js/emojisplosion.js
@@ -1,31 +1,31 @@
 window.emojisplosion = emojisplosion;
 
-const triggerEmojisplosion = (event) => {
-    var sign=Math.random()>0.5?1:-1;
-    emojisplosion({
-        emojiCount: 1,
-        physics: {
-            fontSize: 24,
-            gravity: 0.16,
-            framerate: 12,
-            initialVelocities: {
-                x: {
-                    max: 1,
-                    min: -1,
-                },
-                y: {
-                    max: -3,
-                    min: -5,
-                },
-            },
+const triggerEmojisplosion = event => {
+  var sign = Math.random() > 0.5 ? 1 : -1;
+  emojisplosion({
+    emojiCount: 1,
+    physics: {
+      fontSize: 24,
+      gravity: 0.16,
+      framerate: 12,
+      initialVelocities: {
+        x: {
+          max: 1,
+          min: -1
         },
-        position: {
-            x: event.clientX,
-            y: event.clientY,
-        },
-    });
+        y: {
+          max: -3,
+          min: -5
+        }
+      }
+    },
+    position: {
+      x: event.clientX,
+      y: event.clientY
+    }
+  });
 };
 
 for (const eventName of ["click", "drag", "touchmove"]) {
-    document.addEventListener(eventName, triggerEmojisplosion);
+  document.addEventListener(eventName, triggerEmojisplosion);
 }
